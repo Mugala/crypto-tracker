e576906620dcf8d256eb61c25b05c5b41c913a2f
mugala <mugalalevi21@gmail.com>
2018-05-30 12:00:09 +0300

    Create database
    This will create the database cryptocurrency and connect it to the project for storing users details

--- cryptocurrency/settings.py
+++ cryptocurrency/settings.py
@@ -73,10 +73,13 @@ WSGI_APPLICATION = 'cryptocurrency.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cryptocurrency',
+        'USER': 'levi',
+        'PASSWORD': 'eyeshadow',
     }
 }
 
